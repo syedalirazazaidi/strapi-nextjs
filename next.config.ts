7b7b76e75f4d14a7e3bb7c14ca5eb9f97e043ef8
Ali Raza <aaliraza007@hotmail.com>
2025-04-21 17:44:21 +0500

    added

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://abc.pehlighiza.akdndhrc.com/api/:path*',
+      },
+    ];
+  },
   /* config options here */
   eslint: {
     ignoreDuringBuilds: true,
   },
+  
 };
 
 export default nextConfig;
