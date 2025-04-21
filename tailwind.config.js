8fd100415af53a1e1b49e12fee5b071d6dde5882
Ali Raza <aaliraza007@hotmail.com>
2025-04-21 19:55:17 +0500

    added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-      './app/**/*.{js,ts,jsx,tsx,mdx}', // For App Router
+      './app/**/*.{js,ts,jsx,tsx,mdx}', // For App Router (if used)
       './pages/**/*.{js,ts,jsx,tsx,mdx}', // For Pages Router
-      './components/**/*.{js,ts,jsx,tsx,mdx}',
+      './components/**/*.{js,ts,jsx,tsx,mdx}', // For components
       './src/**/*.{js,ts,jsx,tsx,mdx}', // If using src/ directory
     ],
     theme: {
